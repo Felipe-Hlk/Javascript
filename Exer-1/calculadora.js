c70c806ae378c785d2f630b430b362449c9d2096
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-08 15:49:43 -0300

    Exerc1

---
+++ Exer-1/calculadora.js
@@ -0,0 +1,12 @@
+var n1 = 8;
+var n2 = 4;
+
+var soma = n1 + n2;
+var subtracao = n1 - n2;
+var multiplicacao = n1 * n2;
+var divisao = n1 / n2;
+
+console.log("resultado da soma: " + soma);
+console.log("resultado da subtração: " + subtracao);
+console.log("resultado da multiplicação: " + multiplicacao);
+console.log("resultado da divisão: " + divisao);
