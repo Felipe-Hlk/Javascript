2d3dc3d62bb7b198ed91c8484f4b878baee0164c
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-08 15:53:07 -0300

    Exers-2

---
+++ Exer-2/Atividade-1.js
@@ -0,0 +1,4 @@
+var salarioUsuario = parseFloat(prompt("Digite o seu salario:"));
+var resultadoSalario = salarioUsuario + (salarioUsuario * (30/100));
+
+console.log("Seu Salario após o calculo de aumento de 30% é:" + resultadoSalario);
---
+++ Exer-2/calculadora.js
@@ -0,0 +1,12 @@
+var primeiroNumero = parseInt(prompt("Digite o primeiro número:"));
+var segundoNumero = parseInt(prompt("Digite o segundo número:"));
+
+var soma = primeiroNumero + segundoNumero;
+var subtracao = primeiroNumero - segundoNumero;
+var multiplicacao = primeiroNumero * segundoNumero;
+var divisao = primeiroNumero / segundoNumero;
+
+console.log("resultado da soma: " + soma);
+console.log("resultado da subtração: " + subtracao);
+console.log("resultado da multiplicação: " + multiplicacao);
+console.log("resultado da divisão: " + divisao);
