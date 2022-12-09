f8a77ac8439c28787575870f938861b35959f9b3
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-09 17:14:56 -0300

    Update ex1.js

--- Aulas/ex1.js
+++ Aulas/ex1.js
@@ -1,3 +1,6 @@
+// Variáveis (let)
+/* armazenar informações para acresentar no código*/
+
 console.log('Variáveis');
 
 let firstname = 'Felipe';
@@ -6,7 +9,13 @@ let lastname = 'Arruda'
 console.log(firstname);
 console.log(lastname);
 
+// Constantes (const)
+/* O const cria uma variável cujo o valor é fixo, ou seja, 
+uma constante somente leitura. Isso não significa que o valor é imutável, 
+apenas que a variável constante não pode ser alterada ou retribuída.*/
 
+const middlename = 'Paula';
 
+console.log(middlename);
 
 
