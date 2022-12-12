1717e2f6955bc2f9c6151f8f82061bd7d71adb25
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-12 20:46:19 -0300

    Update index.js

--- Javascript Basics/lesson2 (variables-let)/index.js
+++ Javascript Basics/lesson2 (variables-let)/index.js
@@ -1,7 +1,14 @@
+console.log()
 let nome = 'Pedro';
+console.log(nome, 'Nasceu em ', 1990);
+console.log(nome, 'conheceu Laura ');
+console.log(nome, 'começou a trabalhar ');
+console.log(nome, 'largou de Laura e foi viajar pelo mundo ');
+console.log()
 
-console.log(nome, ' Nasceu em ', 1984);
-console.log(nome, ' conheceu Laura ');
-console.log(nome, ' começou a trabalhar ');
-console.log(nome, ' largou de Laura e foi viajar pelo mundo ');
-
+nome = 'Laura';
+console.log(nome, 'Nasceu em ', 1999);
+console.log(nome, 'conheceu João ');
+console.log(nome, 'começou a faculdade ');
+console.log(nome, 'conheceu Beatriz ');
+console.log(nome, 'quem realmente largou de Pedro ');
