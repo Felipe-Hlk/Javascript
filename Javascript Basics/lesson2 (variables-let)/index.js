aa8245d96f3351d9a32585cc333f74c9da76508f
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-12 20:36:33 -0300

    lesson variables

---
+++ Javascript Basics/lesson2 (variables-let)/index.js
@@ -0,0 +1,7 @@
+let nome = 'Pedro';
+
+console.log(nome, ' Nasceu em ', 1984);
+console.log(nome, ' conheceu Laura ');
+console.log(nome, ' começou a trabalhar ');
+console.log(nome, ' largou de Laura e foi viajar pelo mundo ');
+
