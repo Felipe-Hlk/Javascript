85aa302c81d2a8c80108839ac0d092b5ce465b8f
Felipe-Hlk <felipearrudappereira@gmail.com>
2022-12-13 11:38:04 -0300

    Update index.js

--- Javascript Basics/lesson3 (Const)/index.js
+++ Javascript Basics/lesson3 (Const)/index.js
@@ -1,14 +1,10 @@
 console.log()
-const nome = 'Pedro';
-console.log(nome, 'Nasceu em',1990);
-console.log(nome, 'conheceu Laura ');
-console.log(nome, 'começou a trabalhar ');
-console.log(nome, 'largou de Laura e foi viajar pelo mundo ');
-console.log()
+const numeroUm = 7;
+const numeroDois = 25;
+const resultado = numeroUm * numeroDois;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
 
-nome2 = 'Laura';
-console.log(nome2, 'Nasceu em ', 1999);
-console.log(nome2, 'conheceu', nome);
-console.log(nome2, 'começou a faculdade ');
-console.log(nome2, 'conheceu Beatriz ');
-console.log(nome2, 'quem realmente largou de Pedro ');
+
+
